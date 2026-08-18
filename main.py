9323a6a16e7bf28f6338354d0959f886fb42dc64
Myungji Lee <myungjilee0914@gmail.com>
2026-08-18 22:43:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-# main.py 실행 시 while문 + input()으로 메뉴(①도서 등록 ②전체 도서 조회 ③도서 검색 ④대여/반납 처리 ⑤종료) 표시, 입력값에 따라 로직 분기
-# CLI 실행 + 메뉴 선택 + 전체 프로그램 흐름
-# isbn 기준 검색/처리
-from models.base_book import Book
+
+
+import time # 대여 반납 처리시 처리 시간 기록용
+from models.base_book import Book 
 from models.specialized_books import PaperBook, Ebook
 from utils.helpers import get_string, get_valid_integer, isbn_check
 
+def main():
+        book_catalog = {} # 도서 리스트 담을 딕셔너리 -- base_book.Book class와 연결
+        
+# 대여/반납 여부 리스트
+# 통계
+        
+        while True:
+                print("=" * 20)
+                print("도서 관리 시스템")
+                print("=" * 20)
+                print("원하는 메뉴를 선택해 주세요.")
+                print("=" * 20)
+                print("① 도서 등록")
+                print("② 전체 도서 조회") 
+                print("③ 도서 검색")
+                print("④ 대여/반납 처리") # 대여·반납 시 발생 데이터(ISBN, 처리 시간 등)를 변경 불가능한 **튜플(Tuple)**로 묶어 **리스트(List)**에 순차 저장
+                print("⑤ 통계 조회") # 월간 대여 통계, 최다 대여 도서 목록 콘솔 출력
+                print("⑥ 종료")  
+
+                menu = get_valid_integer(input(원하는 메뉴 번호를 입력하세요: ))
+                if menu == 1: # 도서 등록 -> 책 제목, 작가명, isbn 입력 -> isbn_check()로 isbn 검증 -> book_catalog, isbn_list에 저장
+
+                elif menu == 2: # 전체 도서 조회 -> isbn, 책 제목, 작가명 쫘르륵 book_catalog 객체 하나씩 조회
+
+                elif menu == 3: # 도서 검색 -> book isbn 비교 
+
+                elif menu == 4: # 대여/반납 처리 -> 
+
+                elif menu == 5: # 월간 대여 통계, 최대 대여 도서 목록 콘솔 출력
+
+                elif menu == 6:
+                print("도서 관리 시스템을 종료합니다.")
+                break
+
+
+                        
+                        
+                
 
 if __name__ == "__main__":
         main()
