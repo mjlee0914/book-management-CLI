473b280763730944198845975998bf9dd5bb3584
Myungji Lee <myungjilee0914@gmail.com>
2026-08-18 22:58:04 +0900

    Add is_rented parameter to book constructors
    Updated constructors of PaperBook and Ebook classes to include is_rented parameter.

--- models/specialized_books.py
+++ models/specialized_books.py
@@ -3,12 +3,12 @@ from .base_book import Book
 
 class PaperBook(Book):
     def __init__(self, title, author, isbn, pages):
-        super().__init__(title, author, isbn)
+        super().__init__(title, author, isbn, is_rented)
         self.__pages = pages
 
 
 class Ebook(Book):
     def __init__(self, title, author, isbn, file_size):
-        super().__init__(title, author, isbn)
+        super().__init__(title, author, isbn, is_rented)
         self.__file_size = file_size
 
