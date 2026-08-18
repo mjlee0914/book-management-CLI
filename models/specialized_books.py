ddcbe0b36acbc99bd6baa27eb69820e9ec1e52d7
Myungji Lee <myungjilee0914@gmail.com>
2026-08-18 22:18:16 +0900

    Update specialized_books.py

--- models/specialized_books.py
+++ models/specialized_books.py
@@ -1,10 +1,10 @@
 from .base_book import Book
+
+
 class PaperBook(Book):
     def __init__(self, title, author, isbn, pages):
         super().__init__(title, author, isbn)
         self.__pages = pages
-
-
 
 
 class Ebook(Book):
